89f223f900f5760a996acebfb77bc08a0a7a4c18
Levi Williams <ljw758@hotmail.com>
2023-02-06 16:56:56 -0500

    Testing Git

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's see if this uploads to GitHub!");
+        }
+    }
+}
